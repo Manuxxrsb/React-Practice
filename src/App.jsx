a5aa93a8d780ad601a04463600f709418f708fd4
Manuxxrsb <manuxxrsb@gmail.com>
2024-11-30 18:02:02 -0300

    useState , IIFE Practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 //import Componentes
-import Saludo from "./components/Saludo.jsx";
+import Saludo from "./Components/Saludo.jsx";
+import Boton from "./Components/Boton.jsx";
 
 function App() {
   return (
     <>
       <Saludo />
+      <Boton />
     </>
   );
 }
